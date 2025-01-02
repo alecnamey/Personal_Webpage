8dcffc92f10519f508e38f76fb505e2f3a723c21
3ampotatochip <46552680+3ampotatochip@users.noreply.github.com>
2025-01-02 09:05:01 -0800

    chnages

--- my-react-app1/src/components/Card.jsx
+++ my-react-app1/src/components/Card.jsx
@@ -1,9 +1,9 @@
-import profilePic from '/Users/alecnamey/Personal_Webpage/my-react-app1/src/assets/pfp2.jpeg'
+import profilePic from 'my-react-app1/src/assets/pfp2.jpeg'
 
 function Card(){
     return(
         <div className="card">
-            <img className = "card-image" src ={profilePic} alt="profile picture"></img>
+            <img className = "card-image" src ={profilePic} alt="profilePIC"></img>
         </div>
     );
 }
--- my-react-app1/src/components/Header.jsx
+++ my-react-app1/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import './Header.css';
-import profilePic2 from '/Users/alecnamey/Personal_Webpage/my-react-app1/src/assets/linked.jpeg'
+import profilePic2 from 'my-react-app1/src/assets/linked.jpeg';
 function Header(){
     return(
         <header>
@@ -9,7 +9,7 @@ function Header(){
                     <li><a href="\about">About</a></li>
                     <li><a href="\projects">Projects</a></li>
                     <li><a href="https://www.linkedin.com/in/alec-namey-a03263263/">
-                        <img className = "card-image2" src ={profilePic2} alt="profile picture"></img></a></li>
+                        <img className = "card-image2" src ={profilePic2} alt="linkedinPHT"></img></a></li>
                     <li><a href="\contact">Contact</a></li>
                 </ul>
             </nav>
