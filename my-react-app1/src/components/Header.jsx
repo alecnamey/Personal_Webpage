c13972084fa440f6062bc6c6f26ea5e530a67f0b
Alec Namey <144381448+alecnamey@users.noreply.github.com>
2025-01-02 09:04:09 -0800

    added github to the page

--- my-react-app1/src/components/Header.jsx
+++ my-react-app1/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import './Header.css';
-import profilePic2 from '/Users/alecnamey/Personal_Webpage/my-react-app1/src/assets/linked.jpeg'
+import linkedinpic from '/Users/alecnamey/Personal_Webpage/my-react-app1/src/assets/linkedinit.png'
+import githubpic from '/Users/alecnamey/Personal_Webpage/my-react-app1/src/assets/git.png'
 function Header(){
     return(
         <header>
@@ -8,9 +9,11 @@ function Header(){
                 <ul>
                     <li><a href="\about">About</a></li>
                     <li><a href="\projects">Projects</a></li>
-                    <li><a href="https://www.linkedin.com/in/alec-namey-a03263263/">
-                        <img className = "card-image2" src ={profilePic2} alt="profile picture"></img></a></li>
                     <li><a href="\contact">Contact</a></li>
+                    <li><a href="https://www.linkedin.com/in/alec-namey-a03263263/">
+                    <img className = "card-image2" src ={linkedinpic} alt="linkedin picture"></img></a></li>
+                    <li><a href="https://github.com/alecnamey?tab=repositories">
+                    <img className = "git-image1" src ={githubpic} alt="github picture"></img></a></li>
                 </ul>
             </nav>
 
