5c6b360880a4272fdffec61acf6fd769d5456bb2
3ampotatochip <46552680+3ampotatochip@users.noreply.github.com>
2025-01-02 09:08:51 -0800

    profile pic change

--- my-react-app1/src/components/Header.jsx
+++ my-react-app1/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import './Header.css';
-import linkedinpic from '/Users/alecnamey/Personal_Webpage/my-react-app1/src/assets/linkedinit.png'
-import githubpic from '/Users/alecnamey/Personal_Webpage/my-react-app1/src/assets/git.png'
+import linkedinpic from 'my-react-app1/src/assets/linkedinit.png'
+import githubpic from 'my-react-app1/src/assets/git.png'
 function Header(){
     return(
         <header>
@@ -11,9 +11,9 @@ function Header(){
                     <li><a href="\projects">Projects</a></li>
                     <li><a href="\contact">Contact</a></li>
                     <li><a href="https://www.linkedin.com/in/alec-namey-a03263263/">
-                    <img className = "card-image2" src ={linkedinpic} alt="linkedin picture"></img></a></li>
+                    <img className = "card-image2" src ={linkedinpic} alt="linkedINpic"></img></a></li>
                     <li><a href="https://github.com/alecnamey?tab=repositories">
-                    <img className = "git-image1" src ={githubpic} alt="github picture"></img></a></li>
+                    <img className = "git-image1" src ={githubpic} alt="githubPIC"></img></a></li>
                 </ul>
             </nav>
 
